dccc7a71fa32cb2a08b8b94631eace9d7b7dc635
t0shifollow <marcindymek24@gmail.com>
2018-11-19 19:32:36 +0100

    next commit

--- server/index.js
+++ server/index.js
@@ -14,6 +14,18 @@ app.get('/', (request, response) => {
     });
 });
 
+app.use((request, response, next) =>{
+    const error = new Error('not found');
+    response.status(404);
+    next(error);
+})
+
+app.use((error, request, response, next) => {
+    response.status(response.statusCode || 500);
+    response.json({
+        message: error.message
+    });
+})
 app.listen(5000, () => {
     console.log('Listering on port 5000');
 });
